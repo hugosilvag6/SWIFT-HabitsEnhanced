12d6429816a930f72e2f9b5fca0b76676e2058a8
hugosilvag6 <hugosilvag6@hotmail.com>
2022-07-11 18:58:56 -0300

    Initial Commit

---
+++ HabitsEnhanced/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitsEnhanced
+//
+//  Created by Hugo Silva on 11/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HabitsEnhanced/HabitsEnhancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsEnhancedApp.swift
+//  HabitsEnhanced
+//
+//  Created by Hugo Silva on 11/07/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitsEnhancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
